b362a12764560f283b79c978cbdb24c6268cea5b
Victor Alvarado <vito8916@gmail.com>
2024-07-10 21:02:02 +0100

    refactor: main page code

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,37 +1,34 @@
 import MapTabs from '@/components/MapTabs';
-import Navbar from '@/components/Navbar'
 import GuestFormFormik from "@/components/GuestFormFormik";
-import Header from "@/components/Header";
-import { useTranslations } from "next-intl";
+import {useTranslations} from "next-intl";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
-  const t = useTranslations("Home");
+    const t = useTranslations("Home");
 
-  return (
-    <>
-    <Navbar />
-      <Header />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-3xl">
-          {t("title")}
-        </h1>
-        <h2 className="text-3xl">
-          {t("sections.church")}
-        
-       
-     
-      <MapTabs />
+    return (
+        <>
+            <Navbar />
+            <main className="flex min-h-screen flex-col items-center justify-between p-24">
+                <h1 className="text-3xl">
+                    {t("title")}
+                </h1>
+                <h2 className="text-3xl">
+                    {t("sections.church")}
 
-        </h2>
-        <h2 className="text-3xl">
-          {t("sections.farm")}
-          
-        </h2>
-        <h1>
-          {t("sections.event_registration")}
-        </h1>
-        <GuestFormFormik/>
-      </main>
-    </>
-  );
+
+                    <MapTabs/>
+
+                </h2>
+                <h2 className="text-3xl">
+                    {t("sections.farm")}
+
+                </h2>
+                <h1>
+                    {t("sections.event_registration")}
+                </h1>
+                <GuestFormFormik/>
+            </main>
+        </>
+    );
 }
