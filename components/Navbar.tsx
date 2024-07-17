3e8f8cb54305bde2140577e11f98b370c5dbc903
Victor Alvarado <vito8916@gmail.com>
2024-07-17 04:01:45 +0200

    feat: adding logos and changing style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,30 @@
 'use client'
 
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import Image from 'next/image';
 
 const Navbar = () => {
     return (
 
 
-        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <nav className="bg-background border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <span
                         className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
                     >
-                        CODEBox
+                        <Image
+                            width={50}
+                            height={50}
+                            src="/isotipo_transparent.svg"
+                            alt={"Logo de boda de Adrian y Ana"}
+                        />
                     </span>
                 </a>
                 <div className="flex items-center md:order-2 space-x-1 md:space-x-0 rtl:space-x-reverse">
                     <Select defaultValue={"english"}>
                         <SelectTrigger className="w-[180px]">
-                            <SelectValue defaultValue="" placeholder="Select a language" />
+                            <SelectValue defaultValue="" placeholder="Select a language"/>
                         </SelectTrigger>
                         <SelectContent>
                             <SelectItem value="english">
@@ -73,27 +79,15 @@ const Navbar = () => {
 
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
                      id="navbar-language">
-                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul className="flex flex-col tracking-[0.25em] font-normal p-4 md:p-0 mt-4 border border-gray-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-700">
                         <li>
-                            <a href="#"
-                               className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                            <a href="/"
+                               className="block py-2 px-3 font-bold"
                                aria-current="page">Home</a>
                         </li>
                         <li>
-                            <a href="#"
-                               className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                               className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                               className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Pricing</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                               className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                            <a href="#greeting"
+                               className="block py-2 px-3 ">About</a>
                         </li>
                     </ul>
                 </div>
