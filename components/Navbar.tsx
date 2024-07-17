0c81d002b04ca4fee0ade121a66ce2892dd401e4
Victor Alvarado <vito8916@gmail.com>
2024-07-17 19:47:29 +0200

    refactor: add more menu items

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -79,7 +79,7 @@ const Navbar = () => {
 
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
                      id="navbar-language">
-                    <ul className="flex flex-col tracking-[0.25em] font-normal p-4 md:p-0 mt-4 border border-gray-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-700">
+                    <ul className="flex flex-col tracking-[0.25em] font-normal p-4 md:p-0 mt-4 border border-gray-100 md:space-x-4 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-700">
                         <li>
                             <a href="/"
                                className="block py-2 px-3 font-bold"
@@ -87,7 +87,23 @@ const Navbar = () => {
                         </li>
                         <li>
                             <a href="#"
-                               className="block py-2 px-3 ">About</a>
+                               className="block py-2 px-3 ">Greeting</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                               className="block py-2 px-3 ">About Us</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                               className="block py-2 px-3 ">Gallery</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                               className="block py-2 px-3 ">Locations</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                               className="block py-2 px-3 ">RSVP</a>
                         </li>
                     </ul>
                 </div>
