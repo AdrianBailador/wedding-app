8cda14699503813ff33f86c9aaaea18808c89900
Victor Alvarado <vito8916@gmail.com>
2024-07-27 18:24:06 +0100

    feat: add new extended styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,12 +12,42 @@ const config = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
       screens: {
-        "2xl": "1400px",
+        sm: "100%",
+        md: "100%",
+        lg: "720px",
+        xl: "920px"
+      },
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1rem',
+        md: '2rem',
+        lg: '2rem',
       },
     },
     extend: {
+      gridTemplateColumns: {
+        'custom-postcard': '1fr 50px 50px 1fr',
+      },
+      gridAutoColumns: {
+        '1fr': '1fr',
+      },
+      height: {
+        '80vh': '80vh',
+        '70vw': '70vw',
+      },
+      minHeight: {
+        'auto': 'auto',
+      },
+      spacing: {
+        '4.5': '18px',
+        '2.5': '10px',
+        '3.5': '14px',
+        '18': '76px',
+        '14.5': '58px',
+        '16.5': '66px',
+        '13.5': '54px',
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
