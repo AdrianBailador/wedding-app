0608bd343537463eccdabb70590f40642d64608f
Victor Alvarado <vito8916@gmail.com>
2024-07-27 18:25:00 +0100

    refactor: refactoring the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
-
-import { useState } from 'react'; // Importa useState si no está ya importado en tu archivo
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {useState} from 'react'; // Importa useState si no está ya importado en tu archivo
 import Image from 'next/image';
 import LocalSwitcher from '../components/local-switcher';
 
@@ -12,63 +12,104 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="bg-background border-gray-200 dark:bg-gray-900">
-            <div className="max-w-screen-xl mx-auto px-4">
-                <div className="flex justify-between items-center py-4">
-                    <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                            <Image
-                                width={50}
-                                height={50}
-                                src="/isotipo_transparent.svg"
-                                alt="Logo de boda de Adrian y Ana"
-                            />
-                        </span>
-                    </a>
+        <nav className="bg-background border-gray-200 dark:bg-gray-900 relative lg:sticky top-0 w-full z-30">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <span
+                        className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+                    >
+                        <Image
+                            width={50}
+                            height={50}
+                            src="/isotipo_transparent.svg"
+                            alt={"Logo de boda de Adrian y Ana"}
+                        />
+                    </span>
+                </a>
+                <div className="flex items-center md:order-2 space-x-1 md:space-x-0 rtl:space-x-reverse">
+                    <Select defaultValue={"english"}>
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue defaultValue="" placeholder="Select a language"/>
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="english">
+                                <div className="inline-flex items-center">
+                                    <svg aria-hidden="true" className="h-3.5 w-3.5 rounded-full me-2"
+                                         xmlns="http://www.w3.org/2000/svg" id="flag-icon-css-us"
+                                         viewBox="0 0 512 512">
+                                        <g fillRule="evenodd">
+                                            <g strokeWidth="1pt">
+                                                <path fill="#bd3d44"
+                                                      d="M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
+                                                      transform="scale(3.9385)"/>
+                                                <path fill="#fff"
+                                                      d="M0 10h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
+                                                      transform="scale(3.9385)"/>
+                                            </g>
+                                            <path fill="#192f5d" d="M0 0h98.8v70H0z" transform="scale(3.9385)"/>
+                                            <path fill="#fff"
+                                                  d="M8.2 3l1 2.8H12L9.7 7.5l.9 2.7-2.4-1.7L6 10.2l.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7L74 8.5l-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 7.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 24.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 21.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 38.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 35.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 52.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 49.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 66.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 63.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9z"
+                                                  transform="scale(3.9385)"/>
+                                        </g>
+                                    </svg>
+                                    English (US)
+                                </div>
+                            </SelectItem>
+                            <SelectItem value="deutsch">
+                                <div className="inline-flex items-center">
+                                    <svg className="h-3.5 w-3.5 rounded-full me-2" aria-hidden="true"
+                                         xmlns="http://www.w3.org/2000/svg" id="flag-icon-css-de"
+                                         viewBox="0 0 512 512">
+                                        <path fill="#ffce00" d="M0 341.3h512V512H0z"/>
+                                        <path d="M0 0h512v170.7H0z"/>
+                                        <path fill="#d00" d="M0 170.7h512v170.6H0z"/>
+                                    </svg>
+                                    Deutsch
+                                </div>
+                            </SelectItem>
+                        </SelectContent>
+                    </Select>
 
-                    {/* Botón de hamburguesa para dispositivos móviles */}
-                    <div className="md:hidden">
-                        <button
-                            onClick={toggleMenu}
-                            className="block text-gray-800 dark:text-white focus:outline-none">
-                            <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M4 6H20M4 12H20M4 18H20"
-                                    stroke="currentColor"
-                                    strokeWidth="2"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                />
-                            </svg>
-                        </button>
-                    </div>
+                    <button onClick={toggleMenu} data-collapse-toggle="navbar-language" type="button"
+                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-full md:hidden bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            aria-controls="navbar-language" aria-expanded={isOpen}>
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                             viewBox="0 0 17 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                  d="M1 1h15M1 7h15M1 13h15"/>
+                        </svg>
+                    </button>
+                </div>
 
-                    {/* Menú de navegación */}
-                    <div className={`md:flex md:items-center md:w-auto ${isOpen ? 'block' : 'hidden'}`}>
-                        <ul className="flex flex-col md:flex-row md:space-x-4 md:border-0 md:dark:border-gray-700">
-                            <li>
-                                <a href="/" className="block py-2 px-3 font-bold" aria-current="page">Home</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 px-3">Greeting</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 px-3">About Us</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 px-3">Gallery</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 px-3">Locations</a>
-                            </li>
-                            <li>
-                                <a href="#" className="block py-2 px-3">RSVP</a>
-                            </li>
-                            <li className="flex items-center">
-                                <LocalSwitcher />
-                            </li>
-                        </ul>
-                    </div>
+                <div className={`items-center justify-between ${isOpen ? 'block' : 'hidden'} w-full md:flex md:w-auto md:order-1`}
+                     id="navbar-language">
+                    <ul className="flex flex-col tracking-[0.25em] font-normal p-4 md:p-0 mt-4 border border-gray-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-700">
+                        <li>
+                            <a href="/"
+                               className="block py-2 px-3 font-bold"
+                               aria-current="page">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 ">Greeting</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                               className="block py-2 px-3 ">About Us</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                               className="block py-2 px-3 ">Gallery</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                               className="block py-2 px-3 ">Locations</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                               className="block py-2 px-3 ">RSVP</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
