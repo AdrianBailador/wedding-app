67289a1ebd558744530e2032c8ceaa38e06aea1f
Adrian Bailador <adrianbailador@hotmail.com>
2024-07-28 16:56:05 +0100

    feat: fix index.js

--- static_content/index.js
+++ static_content/index.js
@@ -33,52 +33,95 @@ export const navigation = [
 export const galleryImages = [
     {
         id: "1",
-        url: "/images/gallery/1.jpg",
+        url: "/images/gallery/img1.jpg",
         title: "Image 1",
     },
     {
         id: "2",
-        url: "/images/gallery/2.jpg",
+        url: "/images/gallery/img2.jpg",
         title: "Image 2",
     },
     {
         id: "3",
-        url: "/images/gallery/3.jpg",
+        url: "/images/gallery/img3.jpg",
         title: "Image 3",
     },
     {
         id: "4",
-        url: "/images/gallery/4.jpg",
+        url: "/images/gallery/img4.jpg",
         title: "Image 4",
     },
     {
-        id: "5",
-        url: "/images/gallery/5.jpg",
-        title: "Image 5",
+        id: "13",
+        url: "/images/gallery/img13.jpg",
+        title: "Image 13",
     },
+    
     {
         id: "6",
-        url: "/images/gallery/6.jpg",
+        url: "/images/gallery/img6.jpg",
         title: "Image 6",
     },
     {
         id: "7",
-        url: "/images/gallery/7.jpg",
+        url: "/images/gallery/img7.jpg",
         title: "Image 7",
     },
     {
-        id: "8",
-        url: "/images/gallery/8.jpg",
-        title: "Image 8",
-    },
+        id: "12",
+        url: "/images/gallery/img12.jpg",
+        title: "Image 12",
+    },  
+   
     {
         id: "9",
-        url: "/images/gallery/9.jpg",
+        url: "/images/gallery/img9.jpg",
         title: "Image 9",
     },
     {
         id: "10",
-        url: "/images/gallery/10.jpg",
+        url: "/images/gallery/img10.jpg",
         title: "Image 10",
-    }
+    },
+    {
+        id: "11",
+        url: "/images/gallery/img11.jpg",
+        title: "Image 11",
+    },  
+    {
+        id: "8",
+        url: "/images/gallery/img8.jpg",
+        title: "Image 8",
+    },
+  
+    {
+        id: "5",
+        url: "/images/gallery/img5.jpg",
+        title: "Image 5",
+    },
+    {
+        id: "14",
+        url: "/images/gallery/img14.jpg",
+        title: "Image 14",
+    },
+    {
+        id: "15",
+        url: "/images/gallery/img15.jpg",
+        title: "Image 15",
+    },
+    {
+        id: "16",
+        url: "/images/gallery/img16.jpg",
+        title: "Image 16",
+    },
+    {
+        id: "17",
+        url: "/images/gallery/img17.jpg",
+        title: "Image 17",
+    },
+    {
+        id: "15",
+        url: "/images/gallery/img18.jpg",
+        title: "Image 18",
+    },
 ];
