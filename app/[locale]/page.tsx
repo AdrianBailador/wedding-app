d8e814c3639134fc2711a9a6f5690160dbf115b1
Adrian Bailador <adrianbailador@hotmail.com>
2024-07-28 19:56:41 +0100

    feat: mobile style

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,4 @@
-import {marcellus} from "@/styles/fonts";
+import { marcellus } from "@/styles/fonts";
 import Image from "next/image";
 import Gretting from '@/components/Gretting';
 import img5 from '@/public/images/img5.jpg';
@@ -12,8 +12,7 @@ import MapTabs from '@/components/MapTabs';
 import GuestFormFormik from "@/components/GuestFormFormikEs";
 import GuestFormFormikEn from "@/components/GuestFormFormikEn"; // Importa tu componente en inglés
 import { useLocale, useTranslations } from "next-intl";
-import { useRouter } from 'next/navigation';
-import {galleryImages} from "@/static_content"; 
+import { galleryImages } from "@/static_content";
 
 export default function Home() {
     const t = useTranslations('IndexPage');
@@ -21,32 +20,30 @@ export default function Home() {
 
     return (
         <>
-            <section id="hero"
-                     className="flex flex-col justify-center items-center  px-5 pb-13.5 sm:pb-14.5 md:pb-16.5 lg:pb-18">
+            <section id="hero" className="flex flex-col justify-center items-center px-5 pb-13.5 sm:pb-14.5 md:pb-16.5 lg:pb-18">
                 <div className="container-wide w-full max-w-[1200px] flex-col items-center flex;">
                     <div className="hero-content w-full text-center flex-col items-center flex relative mb-10">
-                        <div className="hero-gallery w-full grid grid-cols-3  xl:gap-5 lg:gap-4.5 md:gap-3.5 gap-1">
+                        <div className="hero-gallery w-full grid grid-cols-3 xl:gap-5 lg:gap-4.5 md:gap-3.5 gap-1">
                             <div className="hero-gallery-item w-full overflow-hidden relative">
                                 <Image
                                     className="w-full object-cover h-[70vw] md:h-80vh min-h-auto md:min-h-[600px]"
                                     src={img9}
-                                    alt="Hero 1"/>
+                                    alt="Hero 1" />
                             </div>
                             <div className="hero-gallery-item w-full overflow-hidden relative">
                                 <Image
                                     className="w-full object-cover h-[70vw] md:h-80vh min-h-auto md:min-h-[600px]"
                                     src={img5}
-                                    alt="Hero 2"/>
+                                    alt="Hero 2" />
                             </div>
                             <div className="hero-gallery-item w-full overflow-hidden relative">
                                 <Image
                                     className="w-full object-cover h-[70vw] md:h-80vh min-h-auto md:min-h-[600px]"
                                     src={img10}
-                                    alt="Hero 3"/>
+                                    alt="Hero 3" />
                             </div>
-
                         </div>
-                        <CardHero/>
+                        <CardHero />
                     </div>
                 </div>
             </section>
@@ -68,7 +65,7 @@ export default function Home() {
                             </h2>
                             <p>{t('sections.locationsText1')}</p>
                         </div>
-                        <Gretting/>
+                        <Gretting />
                     </div>
                 </div>
             </section>
@@ -76,7 +73,7 @@ export default function Home() {
             <section id="about-us" className="section-styles container-full">
                 <div className="content w-full flex flex-col items-center">
                     <div className="grid w-full gap-x-0 gap-y-0 grid-rows-[auto] grid-cols-[1fr_50px_50px_1fr]">
-                        <PostcardBlockRight/>
+                        <PostcardBlockRight />
                         <div
                             className="postcard---image relative flex justify-center items-stretch w-full"
                             style={{
@@ -106,23 +103,21 @@ export default function Home() {
             <section id="localitations" className="flex-col justify-center items-center flex pb-[76px] px-5">
                 <div className="container-wide w-full max-w-[1200px] flex-col items-center flex;">
                     <div className="guest-form-content w-full text-center flex-col items-center flex relative mb-10">
-                    <Image
-                                    src={rama}
-                                    width={80}
-                                    alt="Rama"
-                                    className="h-auto object-contain mb-[14px]"
-                                />
+                        <Image
+                            src={rama}
+                            width={80}
+                            alt="Rama"
+                            className="h-auto object-contain mb-[14px]"
+                        />
                         <br></br>
                         <h2
-                                        style={marcellus.style}
-                                        className="text-[23px] md:text-[32px] font-normal md:leading-[50.16px] text-center">
-                                        {t('sections.locationsText')}
-                                    </h2>
+                            style={marcellus.style}
+                            className="text-[23px] md:text-[32px] font-normal md:leading-[50.16px] text-center">
+                            {t('sections.locationsText')}
+                        </h2>
                         <br></br>
-                         <h2>{t('sections.locations1')}</h2>
-
-                        <MapTabs/>
-
+                        <h2>{t('sections.locations1')}</h2>
+                        <MapTabs />
                     </div>
                 </div>
             </section>
@@ -150,9 +145,9 @@ export default function Home() {
                             </div>
                         </div>
                     </div>
-                    <div className="gallery-mansory columns-6 max-w-7xl mx-auto space-y-4">
-                        {galleryImages.map((el) => (
-                            <div key={el.id} className=' overflow-hidden'>
+                    <div className="gallery-masonry columns-6 max-w-7xl mx-auto space-y-4">
+                        {galleryImages.map((el, index) => (
+                            <div key={el.id} className='gallery-item overflow-hidden'>
                                 <Image
                                     src={el.url}
                                     alt={el.title}
@@ -168,12 +163,12 @@ export default function Home() {
             <section id="guestFormFormik" className="flex-col justify-center items-center flex pb-[76px] px-5">
                 <div className="container-wide w-full max-w-[1200px] flex-col items-center flex;">
                     <div className="guest-form-content w-full text-center flex-col items-center flex relative mb-10">
-                    <Image
-                                    src={rama}
-                                    width={80}
-                                    alt="Rama"
-                                    className="h-auto object-contain mb-[14px]"
-                                />
+                        <Image
+                            src={rama}
+                            width={80}
+                            alt="Rama"
+                            className="h-auto object-contain mb-[14px]"
+                        />
                         <br></br>
                         <h1 className="font-montserrat text-[24px] font-normal leading-[29.26px] text-center mt-2"
                             style={{
@@ -183,7 +178,7 @@ export default function Home() {
                                 border: '0.5px solid transparent',
                                 opacity: 1
                             }}>
-                             {t('sections.event_registration')}
+                            {t('sections.event_registration')}
                         </h1>
                         <br></br>
                         {locale === 'es' ? <GuestFormFormik /> : <GuestFormFormikEn />}
