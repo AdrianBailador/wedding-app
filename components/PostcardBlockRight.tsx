bc5fd772db1179582f3d87ea590d3a02c7761170
Victor Alvarado <vito8916@gmail.com>
2024-07-31 22:01:11 +0100

    fix: huge padding for small devices

--- components/PostcardBlockRight.tsx
+++ components/PostcardBlockRight.tsx
@@ -6,8 +6,8 @@ import { useTranslations } from 'next-intl';
 const PostcardBlockRight = () => {
     const t = useTranslations('AboutUs');
     return (
-        <div className="about-us- right md:order-1 derw-full bg-[white] flex-col justify-center items-stretch mb-[-25px] flex mt-[25px] pl-[50px] py-[85px]">
-            <div className="w-full flex-col justify-center items-start flex px-[16%]">
+        <div className="about-us- right md:order-1 derw-full bg-[white] flex-col justify-center items-stretch mb-[-25px] flex mt-[25px] md:pl-[50px] py-[85px]">
+            <div className="w-full flex-col justify-center items-start flex px-[10%] md:px-[16%]">
                 <Image
                     src={rama}
                     alt="Subtitle"
